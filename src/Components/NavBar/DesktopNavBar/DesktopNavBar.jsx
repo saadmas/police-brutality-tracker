400a411ccc95161d25ecb44843436dda19bc0c92
Saad <saadmasood96@hotmail.com>
2020-06-22 23:49:13 -0400

    create separate component for desktop nav

---
+++ src/Components/NavBar/DesktopNavBar/DesktopNavBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallReceivedIcon from '@material-ui/icons/CallReceived';
+import GavelOutlinedIcon from '@material-ui/icons/GavelOutlined';
+
+import './DesktopNavBar.scss';
+
+const DesktopNavBar = () => {
+  return (
+    <nav className="NavBar">
+      <div className="AppTitle">
+        Police Brutality Tracker
+      </div>
+      <div className="NavItem">
+        <a className="NavLink" href="/">
+          <span className="NavLinkText">Timeline</span>
+          <ScheduleIcon className="NavIcon" />
+        </a>
+        <a className="NavLink" href="/about">
+          <span className="NavLinkText">Donations</span>
+          <CallReceivedIcon className="NavIcon" />
+        </a>
+        <a className="NavLink" href="/about">
+          <span className="NavLinkText">Report Incident</span>
+          <GavelOutlinedIcon className="NavIcon" />
+        </a>
+        <a className="NavLink" href="/about">
+          <span className="NavLinkText">About</span>
+          <InfoOutlinedIcon className="NavIcon" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default DesktopNavBar;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import ScheduleIcon from '@material-ui/icons/Schedule';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import CallReceivedIcon from '@material-ui/icons/CallReceived';
-import GavelOutlinedIcon from '@material-ui/icons/GavelOutlined';
 
+import DesktopNavBar from './DesktopNavBar/DesktopNavBar';
 import useWindowDimensions from '../../Hooks/useWindowDimensions';
 
 import './NavBar.scss';
@@ -11,33 +8,7 @@ import './NavBar.scss';
 const NavBar = () => {
   const { width } = useWindowDimensions();
 
-  const getDesktopNav = () => (
-    <nav className="NavBar">
-      <div className="AppTitle">
-        Police Brutality Tracker
-      </div>
-      <div className="NavItem">
-        <a className="NavLink" href="/">
-          <span className="NavLinkText">Timeline</span>
-          <ScheduleIcon className="NavIcon" />
-        </a>
-        <a className="NavLink" href="/about">
-          <span className="NavLinkText">Donations</span>
-          <CallReceivedIcon className="NavIcon" />
-        </a>
-        <a className="NavLink" href="/about">
-          <span className="NavLinkText">Report Incident</span>
-          <GavelOutlinedIcon className="NavIcon" />
-        </a>
-        <a className="NavLink" href="/about">
-          <span className="NavLinkText">About</span>
-          <InfoOutlinedIcon className="NavIcon" />
-        </a>
-      </div>
-    </nav>
-  );
-
-  return getDesktopNav();
+  return <DesktopNavBar />;
 };
 
 export default NavBar;
