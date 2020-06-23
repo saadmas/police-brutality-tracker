905c1fce23b460c5c9be8f656d4d109d56fd0584
Saad <saadmasood96@hotmail.com>
2020-06-23 18:20:10 -0400

    Links instead of anchor tags in Desktop nav bar

--- src/Components/NavBar/DesktopNavBar/DesktopNavBar.jsx
+++ src/Components/NavBar/DesktopNavBar/DesktopNavBar.jsx
@@ -3,6 +3,7 @@ import ScheduleIcon from '@material-ui/icons/Schedule';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import CallReceivedIcon from '@material-ui/icons/CallReceived';
 import GavelOutlinedIcon from '@material-ui/icons/GavelOutlined';
+import { Link } from 'react-router-dom'
 
 import './DesktopNavBar.scss';
 
@@ -13,22 +14,22 @@ const DesktopNavBar = () => {
         Police Brutality Tracker
       </div>
       <div className="NavItem">
-        <a className="NavLink" href="/">
+        <Link className="NavLink" to="/">
           <span className="NavLinkText">Timeline</span>
           <ScheduleIcon className="NavIcon" />
-        </a>
-        <a className="NavLink" href="/about">
+        </Link>
+        <Link className="NavLink" to="/about">
           <span className="NavLinkText">Donations</span>
           <CallReceivedIcon className="NavIcon" />
-        </a>
-        <a className="NavLink" href="/about">
+        </Link>
+        <Link className="NavLink" to="/about">
           <span className="NavLinkText">Report Incident</span>
           <GavelOutlinedIcon className="NavIcon" />
-        </a>
-        <a className="NavLink" href="/about">
+        </Link>
+        <Link className="NavLink" to="/about">
           <span className="NavLinkText">About</span>
           <InfoOutlinedIcon className="NavIcon" />
-        </a>
+        </Link>
       </div>
     </nav>
   );
