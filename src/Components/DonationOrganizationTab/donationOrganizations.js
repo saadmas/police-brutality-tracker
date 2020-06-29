650be82890c39c089879ef89f095e130278b88c0
Saad <saadmasood96@hotmail.com>
2020-06-29 00:08:13 -0400

    make list of donation organizations

--- src/Components/DonationOrganizationTab/donationOrganizations.js
+++ src/Components/DonationOrganizationTab/donationOrganizations.js
@@ -1,3 +1,70 @@
 const donationOrganizations = [
-
+  {
+    name: 'Know Your Rights Camp',
+    url: 'https://secure.eifoundation.org/site/Donation2?df_id=6082&mfc_pref=T&6082.donation=form1&s_src=kyrcmain&utm_source=kyrcmain'
+  },
+  {
+    name: 'Oakland Anti Police Terror Project',
+    url: 'https://www.antipoliceterrorproject.org/donate'
+  },
+  {
+    name: 'American Civil Liberties Union (ACLU)',
+    url: 'https://www.aclu.org/'
+  },
+  {
+    name: 'National Police Accountability Project',
+    url: 'https://www.nlg-npap.org/'
+  },
+  {
+    name: 'Advancement Project',
+    url: 'https://advancementproject.org/'
+  },
+  {
+    name: 'Change The NYPD',
+    url: 'https://www.changethenypd.org/'
+  },
+  {
+    name: 'Movement For Black Lives',
+    url: 'https://m4bl.org/'
+  },
+  {
+    name: 'The Marshall Project',
+    url: 'https://www.themarshallproject.org/'
+  },
+  {
+    name: 'NAACP Legal Defense and Educational Fund (LDF)',
+    url: 'https://org2.salsalabs.com/o/6857/p/salsa/donation/common/public/?donate_page_KEY=15780&_ga=2.66839982.1065874069.1591039493-1035269687.1591039493'
+  },
+  {
+    name: 'Sylvia Rivera Law Project',
+    url: 'https://srlp.org/donate/'
+  },
+  {
+    name: 'BLD PWR',
+    url: 'https://bldpwr.com/about-us'
+  },
+  {
+    name: 'Black Leaders Organizing for Communities (BLOC)',
+    url: 'https://www.blocbybloc.org/'
+  },
+  {
+    name: 'Innocence Project',
+    url: 'https://www.innocenceproject.org/'
+  },
+  {
+    name: 'Equal Justice Initiative',
+    url: 'https://eji.org/'
+  },
+  {
+    name: 'Stono Institute for Freedom, Justice and Security',
+    url: 'https://stonoinstitute.com/donate/'
+  },
+  {
+    name: 'Black Futures Lab',
+    url: 'https://cpasf.ourpowerbase.net/civicrm/contribute/transact?reset=1&id=45'
+  },
+  {
+    name: 'Uniting Wisdom',
+    url: 'https://www.unitingwisdom.com/'
+  }
 ];
